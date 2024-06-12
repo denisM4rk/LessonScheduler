34191279c1469cd3f422192ceeb11e95c3d49bc6
denisbryanmark@gmail.com <denisbryanmark@gmail.com>
2024-06-12 13:36:19 +0300

    PDF-импорт

--- LessonScheduler/Pages/SchedulePage.xaml.cs
+++ LessonScheduler/Pages/SchedulePage.xaml.cs
@@ -1,6 +1,10 @@
-﻿using LessonScheduler.AppFiles;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using LessonScheduler.AppFiles;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -142,7 +146,7 @@ namespace LessonScheduler.Pages
 
             if (selectedPlans != null)
             {
-               GridStages.ItemsSource = DbConnect.entObj.Stages.Where(s => s.IdPlan == selectedPlans.Id).ToList();
+                GridStages.ItemsSource = DbConnect.entObj.Stages.Where(s => s.IdPlan == selectedPlans.Id).ToList();
             }
         }
 
@@ -191,6 +195,230 @@ namespace LessonScheduler.Pages
                                     MessageBoxButton.OK,
                                     MessageBoxImage.Warning);
                 }
+            }
+        }
+
+        private void BtnPDF_Click(object sender, RoutedEventArgs e)
+        {
+            selectedPlans = (Plans)GridPlans.SelectedItem;
+
+            try
+            {
+                Document document = new Document();
+
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\План занятия.pdf", FileMode.Create));
+
+                BaseFont baseFont = BaseFont.CreateFont("c:/windows/fonts/arial.ttf", "Identity-H", true);
+                Font font = new Font(baseFont, 12);
+
+                document.Open();
+
+                var plan = DbConnect.entObj.Stages.FirstOrDefault(s => s.IdPlan == selectedPlans.Id);
+
+                iTextSharp.text.Paragraph header = new iTextSharp.text.Paragraph(plan.Plans.LessonTopic, font);
+                header.Alignment = Element.ALIGN_CENTER;
+                header.SpacingBefore = 10f;
+                header.SpacingAfter = 10f;
+                document.Add(header);
+
+                PdfPTable table = new PdfPTable(5);
+
+                Font headerFont = new Font(Font.FontFamily.HELVETICA, 10, Font.BOLD);
+
+                table.AddCell(new PdfPCell(new Phrase("№", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                table.AddCell(new PdfPCell(new Phrase("Длительность", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                table.AddCell(new PdfPCell(new Phrase("Материал", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                table.AddCell(new PdfPCell(new Phrase("Деятельность учителя", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                table.AddCell(new PdfPCell(new Phrase("Деятельность ученика", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+
+                foreach (var row in GridStages.ItemsSource)
+                {
+                    table.AddCell(((Stages)row).StageNumber.ToString());
+                    table.AddCell(((Stages)row).TimeDuration.ToString());
+                    table.AddCell(((Stages)row).Material);
+                    table.AddCell(((Stages)row).TeacherActivity);
+                    table.AddCell(((Stages)row).StudentActivity);
+                }
+                document.Add(table);
+
+                document.Close();
+
+                MessageBox.Show("План экспортирован!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Критическая ошибка при сохранении" + ex, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnSchedulePDF_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Document document = new Document();
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Расписание.pdf", FileMode.Create));
+
+                BaseFont baseFont = BaseFont.CreateFont("c:/windows/fonts/arial.ttf", "Identity-H", true);
+                Font font = new Font(baseFont, 12);
+
+                document.Open();
+
+                // Заголовок учебного класса
+                var header = DbConnect.entObj.Classes.FirstOrDefault(s => s.Id == CmbClass.SelectedIndex + 1);
+                iTextSharp.text.Paragraph headerClass = new iTextSharp.text.Paragraph(header.ClassNumber, font);
+                headerClass.Alignment = Element.ALIGN_CENTER;
+                headerClass.SpacingBefore = 10f;
+                headerClass.SpacingAfter = 10f;
+                document.Add(headerClass);
+
+                // Понедельник
+                var monday = DbConnect.entObj.Days.FirstOrDefault(s => s.Id == 1);
+
+                iTextSharp.text.Paragraph header1 = new iTextSharp.text.Paragraph(monday.DayName, font);
+                header1.Alignment = Element.ALIGN_CENTER;
+                header1.SpacingBefore = 10f;
+                header1.SpacingAfter = 10f;
+                document.Add(header1);
+
+                PdfPTable mondayTable = new PdfPTable(3);
+                mondayTable.AddCell(new PdfPCell(new Phrase("№", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                mondayTable.AddCell(new PdfPCell(new Phrase("Урок", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                mondayTable.AddCell(new PdfPCell(new Phrase("Кабинет", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+
+                foreach (var row in GridMonday.ItemsSource)
+                {
+                    PdfPCell cell = new PdfPCell(new Phrase(((Lessons)row).NumberOfLesson.ToString(), font));
+                    mondayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Lesson, font));
+                    mondayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Cabinet.ToString(), font));
+                    mondayTable.AddCell(cell);
+                }
+
+                document.Add(mondayTable);
+
+                // Вторник
+                var tuesday = DbConnect.entObj.Days.FirstOrDefault(s => s.Id == 2);
+
+                iTextSharp.text.Paragraph header2 = new iTextSharp.text.Paragraph(tuesday.DayName, font);
+                header2.Alignment = Element.ALIGN_CENTER;
+                header2.SpacingBefore = 10f;
+                header2.SpacingAfter = 10f;
+                document.Add(header2);
+
+                PdfPTable tuesdayTable = new PdfPTable(3);
+                tuesdayTable.AddCell(new PdfPCell(new Phrase("№", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                tuesdayTable.AddCell(new PdfPCell(new Phrase("Урок", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                tuesdayTable.AddCell(new PdfPCell(new Phrase("Кабинет", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+
+                foreach (var row in GridTuesday.ItemsSource)
+                {
+                    PdfPCell cell = new PdfPCell(new Phrase(((Lessons)row).NumberOfLesson.ToString(), font));
+                    tuesdayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Lesson, font));
+                    tuesdayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Cabinet.ToString(), font));
+                    tuesdayTable.AddCell(cell);
+                }
+
+                document.Add(tuesdayTable);
+
+                // Среда
+                var wednesday = DbConnect.entObj.Days.FirstOrDefault(s => s.Id == 3);
+
+                iTextSharp.text.Paragraph header3 = new iTextSharp.text.Paragraph(wednesday.DayName, font);
+                header3.Alignment = Element.ALIGN_CENTER;
+                header3.SpacingBefore = 10f;
+                header3.SpacingAfter = 10f;
+                document.Add(header3);
+
+                PdfPTable wednesdayTable = new PdfPTable(3);
+                wednesdayTable.AddCell(new PdfPCell(new Phrase("№", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                wednesdayTable.AddCell(new PdfPCell(new Phrase("Урок", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                wednesdayTable.AddCell(new PdfPCell(new Phrase("Кабинет", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+
+                foreach (var row in GridWednesday.ItemsSource)
+                {
+                    PdfPCell cell = new PdfPCell(new Phrase(((Lessons)row).NumberOfLesson.ToString(), font));
+                    wednesdayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Lesson, font));
+                    wednesdayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Cabinet.ToString(), font));
+                    wednesdayTable.AddCell(cell);
+                }
+
+                document.Add(wednesdayTable);
+
+                // Четверг
+                var thurday = DbConnect.entObj.Days.FirstOrDefault(s => s.Id == 4);
+
+                iTextSharp.text.Paragraph header4 = new iTextSharp.text.Paragraph(thurday.DayName, font);
+                header4.Alignment = Element.ALIGN_CENTER;
+                header4.SpacingBefore = 10f;
+                header4.SpacingAfter = 10f;
+                document.Add(header4);
+
+                PdfPTable thursdayTable = new PdfPTable(3);
+                thursdayTable.AddCell(new PdfPCell(new Phrase("№", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                thursdayTable.AddCell(new PdfPCell(new Phrase("Урок", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                thursdayTable.AddCell(new PdfPCell(new Phrase("Кабинет", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+
+                foreach (var row in GridThursday.ItemsSource)
+                {
+                    PdfPCell cell = new PdfPCell(new Phrase(((Lessons)row).NumberOfLesson.ToString(), font));
+                    thursdayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Lesson, font));
+                    thursdayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Cabinet.ToString(), font));
+                    thursdayTable.AddCell(cell);
+                }
+
+                document.Add(thursdayTable);
+
+                // Пятница
+                var friday = DbConnect.entObj.Days.FirstOrDefault(s => s.Id == 5);
+
+                iTextSharp.text.Paragraph header5 = new iTextSharp.text.Paragraph(friday.DayName, font);
+                header5.Alignment = Element.ALIGN_CENTER;
+                header5.SpacingBefore = 10f;
+                header5.SpacingAfter = 10f;
+                document.Add(header5);
+
+                PdfPTable fridayTable = new PdfPTable(3);
+                fridayTable.AddCell(new PdfPCell(new Phrase("№", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                fridayTable.AddCell(new PdfPCell(new Phrase("Урок", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+                fridayTable.AddCell(new PdfPCell(new Phrase("Кабинет", font)) { BackgroundColor = new BaseColor(240, 240, 240) });
+
+                foreach (var row in GridFriday.ItemsSource)
+                {
+                    PdfPCell cell = new PdfPCell(new Phrase(((Lessons)row).NumberOfLesson.ToString(), font));
+                    fridayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Lesson, font));
+                    fridayTable.AddCell(cell);
+
+                    cell = new PdfPCell(new Phrase(((Lessons)row).Cabinet.ToString(), font));
+                    fridayTable.AddCell(cell);
+                }
+
+                document.Add(fridayTable);
+
+                document.Close();
+
+                MessageBox.Show("Расписание экспортировано!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+    
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Критическая ошибка при сохранении" + ex, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
